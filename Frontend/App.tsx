a7b7cdf363c0c50f6859b3935bb163001065d8ea
SamarthBagde <bagdesamarth70@gmail.com>
2026-07-31 11:25:19 +0530

    starting frontend development

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,17 +1,26 @@
-import { StatusBar } from 'react-native';
-import {
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
-import Home from './src/screen/Home';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
-
   return (
-    <SafeAreaProvider>
-      <StatusBar />
-      <Home />
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text style={styles.text}>Advance Expense Tracker</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0F172A',
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
